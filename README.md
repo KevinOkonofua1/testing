f1bbed1c582c787a2db4fa5c580b707f20a7d085
Kevin Okonofua <101487856+KevinOkonofua1@users.noreply.github.com>
2022-08-27 05:35:50 +0100

    Automating and Testing Screen

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # testing
+Install chromedriver for chrome or any other browser you intend to use.
--- automation_testing.py
+++ automation_testing.py
@@ -11,6 +11,8 @@ print(show_message_botton.get_attribute('innerHTML'))
 assert 'Show Message' in chrome_browser.page_source
 
 user_message = chrome_browser.find_element_by_id('user-message')
+user_button2 = chrome_browser.find_element_by_css_selector('#get-input > .btn')
+print(user_button2)
 user_message.clear()
 user_message.send_keys('I AM COMING TO WORK')
 
@@ -18,3 +20,7 @@ show_message_botton.click()
 
 output_message = chrome_browser.find_element_by_id('display')
 assert 'I AM COMING TO WORK' in output_message.text
+
+chrome_browser.close()
+chrome_browser.close()
+# chrome_browser.quit()
